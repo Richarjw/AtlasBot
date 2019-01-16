4e098e1f35ae88b127ab9a3d0928ccc237be1996
Jack Richard <jackwilliamrichard@gmail.com>
2019-01-16 09:22:17 -0600

    Great

--- atlas.py
+++ atlas.py
@@ -5,38 +5,45 @@ import os
 import sys
 import subprocess
 
+
 class Atlas():
-  def __init__(self):
-    self._kernel = aiml.Kernel()
+    def __init__(self):
+        self._kernel = aiml.Kernel()
+        self.messages = []
+        self.responses = []
 
-  def initialize(self, aiml_dir):
-    self._kernel.learn(os.sep.join([aiml_dir, '*.aiml']))
-    properties_file = open(os.sep.join([aiml_dir, 'bot.properties']))
-    for line in properties_file:
-      parts = line.split('=')
-      key = parts[0]
-      value = parts[1]
-      self._kernel.setBotPredicate(key, value)
+    def initialize(self, aiml_dir):
+        self._kernel.learn(os.sep.join([aiml_dir, '*.aiml']))
+        self.messages = []
+        self.responses = []
+        properties_file = open(os.sep.join([aiml_dir, 'bot.properties']))
+        for line in properties_file:
+            parts = line.split('=')
+            key = parts[0]
+            value = parts[1]
+            self._kernel.setBotPredicate(key, value)
 
-  def respond(self, input):
-    response = self._kernel.respond(input)
-    return response
+    def respond(self, input):
+        response = self._kernel.respond(input)
+        return response
 
-  def speak(self, response):
-    print(response)
-    return
+    def speak(self, response):
+        return response
 
-  def speakResponse(self, input):
-    self.speak(self.respond(input))
-
+    def speakResponse(self, input):
+        str = self.speak(self.respond(input))
+        self.messages.append(input)
+        self.responses.append(str)
+        return str
 
 def main():
-  atlas = Atlas()
-  atlas.initialize("aiml") # parameter is the aiml directory
+    atlas = Atlas()
+    atlas.initialize("aiml")  # parameter is the aiml directory
 
-  while True:
-    n = input("Input: ")
-    atlas.speakResponse(n)
-    
+    while True:
+        n = input("Input: ")
+        atlas.speakResponse(n)
+
+
 if __name__ == '__main__':
-  main()
+    main()
--- server.py
+++ server.py
@@ -1,22 +1,60 @@
 #!/usr/bin/env python
 
-from flask import Flask,flash, request
+from flask import Flask, flash, request, url_for
 from flask import render_template
 import atlas
-
+import time
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+messages = []
+responses = []
+
+history = dict()
+
+
+# @app.route("/question", methods=['POST', 'GET'])
+# def question():
+#     if request.method == 'POST':
+#         flash(request.form['question'])
+#         bot.speakResponse(
+#             request.form['question']))
+#     return render_template("question.html", messages=messages, responses=responses)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def messenger():
+    global history
+    response = ""
+    if request.method == 'POST':
+        if request.form['button'] == 'Submit':
+            response = bot.speakResponse(
+                request.form['question'])
+            history[request.form['question']] = response
+
+        if request.form['button'] == 'Clear':
+            history = dict()
+    return render_template("messenger.html", history=history)
+
+
+# @app.route("/")
+# def home():
+#     return app.send_static_file('index.html')
+
+
+@app.route("/speech-recognition")
+def speechrecognition():
     return app.send_static_file('index.html')
+
 
 @app.route("/respond", methods=['POST'])
 def respond():
     bot.speakResponse(request.form['input'])
-    flash('HEY!')
     return app.send_static_file('index.html')
+
 
 if __name__ == '__main__':
     bot = atlas.Atlas()
     bot.initialize("aiml")
+    history = dict()
     app.run(debug=True, host='127.0.0.1', port=3000)
